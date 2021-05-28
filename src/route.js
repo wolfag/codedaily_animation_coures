edf477e7dae316c07f744e807174d2123ec2e0af
TaiNguyen <tai@useget.com>
2021-05-28 12:36:16 +0700

    config navigation

---
+++ src/route.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import HomeScreen from './HomeScreen';
+
+const Stack = createStackNavigator();
+
+export default function RootNavigate() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
